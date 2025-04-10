d9a613037ebfe7e9976b292e4a308eab4d483d42
jonasyke <jonasyke171@gmail.com>
2025-04-10 11:00:42 -0800

    finish L6

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from stats import get_num_words
-def get_book_text(path):
-	return path.read()
+def main():
+    from stats import get_num_words, letter_count
+    def get_book_text(path):
+        return path.read()
 
 
-with open("books/frankenstein.txt") as f:
-    words = get_book_text(f)
-    word_list = words.split()
-    count = get_num_words(word_list)
+    with open("books/frankenstein.txt") as f:
+        words = get_book_text(f)
+        word_list = words.split()
+        count = get_num_words(word_list)
     print(f"{count} words found in the document")
+    print(letter_count(words))
+
+main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
+
 def get_num_words(words):
     count = len(words)
     return count
+
+def letter_count(words):
+    text = words
+    letters = {} 
+    for char in text:
+        if char.isalpha():
+            lower_char = char.lower()
+            letters[lower_char] = letters.get(lower_char,0) + 1
+        else:
+            letters[char] = letters.get(char,0) + 1
+    return letters
+
+
