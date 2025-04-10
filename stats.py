e7ed80dc7f2fee8a66b21772dab85c20709a40d2
jonasyke <jonasyke171@gmail.com>
2025-04-10 14:39:53 -0800

    Save after creating Organized List

--- stats.py
+++ stats.py
@@ -1,4 +1,3 @@
-
 def get_num_words(words):
     count = len(words)
     return count
@@ -14,4 +13,18 @@ def letter_count(words):
             letters[char] = letters.get(char,0) + 1
     return letters
 
+def second(letters):
+    dict_list =[]
+    for letter in letters:
+        dict_list.append({'character':letter, 'count':letters[letter]})
+    return dict_list
 
+def sort_on(dict_list):
+    return dict_list['count']
+
+def main():
+    words = "This is The Test STRIng"
+    print(sorted(second(letter_count(words)), reverse=True, key=sort_on))
+
+if __name__ == "__main__":
+    main()
